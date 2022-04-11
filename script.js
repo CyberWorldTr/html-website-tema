a70739d2a702ca1fe42c0787dd0a6edde8625e70
CyberWorldTr <cyberworldtur@gmail.com>
2022-04-11 03:50:24 +0300

    dark mode && other changes

--- script.js
+++ script.js
@@ -20,4 +20,17 @@ function validate() {
         }
     }
 }
-
+//Dark Mode Change
+function darkMode() {
+    var element = document.body;
+    var modeIcon = document.getElementById('modeIcon');
+    element.classList.toggle("dark-mode");
+    if(modeIcon.classList.contains("fa-sun-o")){
+        modeIcon.classList.add("fa-moon-o")
+        modeIcon.classList.remove("fa-sun-o");
+      }
+      else{
+        modeIcon.classList.add("fa-sun-o")
+        modeIcon.classList.remove("fa-moon-o");
+      }
+ }
